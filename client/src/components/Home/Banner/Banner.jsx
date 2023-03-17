d5c8fa1fc8c650840dd5e83cfbd2035064d519c4
thaovytvan <thaovyxeo@gmail.com>
2023-03-17 12:40:53 +0700

    update package & added home page ui

--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,24 @@
 import "./Banner.scss";
+import BannerImg from "~/assets/banner-img.png";
 const Banner = () => {
-    return <div>Banner</div>;
+    return (
+    <div className="hero-banner">
+        <div className="content">
+            <div className="text-content">
+                <h1>SALES</h1>
+                <p> Convallis interdum purus adipiscing dis parturient
+                    posuere ac a quam a eleifend montes parturient posuere
+                    curae tempor
+                </p>
+                <div className="more">
+                    <div className="banner-more">Read More</div>
+                    <div className="banner-more shop">Shop Now</div>
+                </div>
+            </div>
+            <img className="banner-img" src={BannerImg} alt="" />
+        </div>
+    </div>
+    );
 };
 
 export default Banner;
